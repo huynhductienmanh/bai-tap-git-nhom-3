4d7594e494b798a815394c8744712dd3e1ff6bdd
ManhLap <huynhductienmanh@gmail.com>
2026-06-23 11:14:02 +0700

    chore: cấu hình hệ thống tự động kiểm thử GitHub Actions

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ def tinh_diem_gpa(diem_so):
     else:
         return round((diem_so / 10) * 4, 2)
 
-print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))
+print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))  
---
+++ test_main.py
@@ -0,0 +1,17 @@
+import unittest
+from main import tinh_diem_gpa
+
+
+class TestGpaFunctions(unittest.TestCase):
+
+    def test_gpa_he_muoi(self):
+        # Kiểm tra điểm 8.5 có trả về 4.0 không
+        self.assertEqual(tinh_diem_gpa(8.5), 4.0)
+
+    def test_gpa_tuyen_tinh(self):
+        # Kiểm tra điểm 7.0 có trả về 2.8 không
+        self.assertEqual(tinh_diem_gpa(7.0), 2.8)
+
+
+if __name__ == '__main__':
+    unittest.main()
